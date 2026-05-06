8202365524370ab4c39d0b8b939648446a3c6008
Mitansh Raithatha <raithathamitansh@gmail.com>
2026-05-06 12:31:21 +0530

    User-defined server.js

--- Backend/server.js
+++ Backend/server.js
@@ -37,46 +37,50 @@ const SECRET_KEY = process.env.JWT_SECRET;
 // ======================
 // SIGNUP
 // ======================
-app.post("/signup", async (req, res) => {
+app.post("/signup", (req, res) => {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
         return res.status(400).json({ message: "All fields required" });
     }
 
-    try {
-        // Check existing user
-        db.query("SELECT * FROM users WHERE email = ?", [email], async (err, result) => {
-            if (err) return res.status(500).json({ message: "Server error" });
+    // Check if user already exists
+    db.query(
+        "SELECT * FROM users WHERE email = ?",
+        [email],
+        (err, result) => {
+            if (err) {
+                console.error("DB ERROR:", err);
+                return res.status(500).json({ message: "Server error" });
+            }
 
             if (result.length > 0) {
                 return res.status(400).json({ message: "Email already exists" });
             }
 
-            const hashedPassword = await bcrypt.hash(password, 10,(err,hachedPassword) => {
-                if(err){
-                    return res.status(500).json({message: "Hashing failed"});
+            // Hash password
+            bcrypt.hash(password, 10, (err, hashedPassword) => {
+                if (err) {
+                    console.error("HASH ERROR:", err);
+                    return res.status(500).json({ message: "Hashing failed" });
                 }
-            });
 
-            // Insert user
-            db.query(
-                "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-                [name, email, hashedPassword],
-                (err) => {
-                    if (err) {
-                         console.error("INSERT ERROR:", err);
-                         return res.status(500).json({ message: "Signup failed" });
+                // Insert user
+                db.query(
+                    "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+                    [name, email, hashedPassword],
+                    (err) => {
+                        if (err) {
+                            console.error("INSERT ERROR:", err);
+                            return res.status(500).json({ message: "Signup failed" });
+                        }
+
+                        res.json({ message: "User registered successfully" });
                     }
-                        
-                    res.json({ message: "User registered successfully" });
-                }
-            );
-        });
-
-    } catch (error) {
-        res.status(500).json({ message: "Something went wrong" });
-    }
+                );
+            });
+        }
+    );
 });
 
 
@@ -133,10 +137,11 @@ app.post("/login", (req, res) => {
 // ADD
 app.post("/add-entry", verifyToken,validateEntry, (req, res) => {
     const { product, quantity, price, total } = req.body;
+    const userId = req.user.id; //comes from JWT
 
     db.query(
-        "INSERT INTO products (product, quantity, price, total) VALUES (?, ?, ?, ?)",
-        [product, quantity, price, total],
+        "INSERT INTO products (user_id, product, quantity, price, total) VALUES (? ,?, ?, ?, ?)",
+        [userId, product, quantity, price, total],
         (err) => {
             if (err) return res.status(500).json({ message: "Error adding entry" });
             res.json({ message: "Added" });
@@ -146,7 +151,12 @@ app.post("/add-entry", verifyToken,validateEntry, (req, res) => {
 
 // GET
 app.get("/entries", verifyToken, (req, res) => {
-    db.query("SELECT * FROM products", (err, result) => {
+
+    const userId = req.user.id;
+
+    db.query("SELECT * FROM products WHERE user_id = ?", 
+        [userId],
+        (err, result) => {
         if (err) return res.status(500).json({ message: "Error fetching data" });
         res.json(result);
     });
@@ -161,8 +171,8 @@ app.put("/update-entry/:id", verifyToken, (req,res)=>{
      console.log("Updated Hit", req.params.id);
 
      db.query(
-        "UPDATE products SET product=? , quantity=? , price=? , total=? WHERE id=?",
-        [product,quantity,price,total,id],
+        "UPDATE products SET product=? , quantity=? , price=? , total=? WHERE id=? AND user_id = ?",
+        [product,quantity,price,total,id,req.user.id],
         (err)=>{
             if(err) return res.status(500).json({message:"Error updating entry"});
             res.json({message: "updated successfully"});
@@ -175,7 +185,9 @@ app.put("/update-entry/:id", verifyToken, (req,res)=>{
 app.delete("/delete-entry/:id", verifyToken, (req, res) => {
     const id = req.params.id;
 
-    db.query("DELETE FROM products WHERE id = ?", [id], (err) => {
+    db.query("DELETE FROM products WHERE id = ? AND user_id = ?", 
+        [id,req.user.id], 
+        (err) => {
         if (err) return res.status(500).json({ message: "Error deleting" });
         res.json({ message: "Deleted" });
     });
